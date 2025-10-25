4c7ffb32de9e8d393e8e1bb4126ed0c19509024a
Shahadat Shikder <shahadatit22@gmail.com>
2025-10-25 20:04:04 +0600

    Remove call button zooming

--- src/components/shared/BottomSection.tsx
+++ src/components/shared/BottomSection.tsx
@@ -5,7 +5,7 @@ import WhatsAppIcon from '../svg/WhatsAppIcon'
 
 const BottomSection = () => {
     return (
-        <div className=' fixed bottom-0 left-0 right-0 grid lg:hidden grid-cols-2'>
+        <div className='mobile-call-button fixed bottom-0 left-0 right-0 grid lg:hidden grid-cols-2'>
             <Link href={'https://wa.me/+8801710060020'} className='w-full py-3 flex items-center justify-center bg-[#075E54] text-white gap-2'>
                 <WhatsAppIcon size={24} />
                 WhatsApp
