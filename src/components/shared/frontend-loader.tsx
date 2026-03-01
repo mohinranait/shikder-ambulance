3aacb820b7917ad8244675fb8132c74fe8b82a09
Md. Ebrahim <ebrahimit49@gmail.com>
2026-03-01 22:27:19 +0600

    removed unused code

--- src/components/shared/frontend-loader.tsx
+++ src/components/shared/frontend-loader.tsx
@@ -1,22 +1,5 @@
 "use client";
-
-import { useState, useEffect } from "react";
-
 export default function FrontendPageLoader() {
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((prev) => {
-        if (prev >= 100) {
-          return 100;
-        }
-        return prev + 1;
-      });
-    }, 40);
-
-    return () => clearInterval(timer);
-  }, []);
 
   return (
     <div className="fixed inset-0 bg-white flex items-center justify-center z-50 overflow-hidden">
