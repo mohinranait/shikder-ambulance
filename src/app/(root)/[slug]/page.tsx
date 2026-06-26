f4689d5aa2bed3f0b7cc0a7806f5584ee61b8463
Md. Ebrahim <ebrahimit49@gmail.com>
2026-06-26 10:50:41 +0600

    remvoed loading for blog pages

--- src/app/(root)/[slug]/page.tsx
+++ src/app/(root)/[slug]/page.tsx
@@ -12,23 +12,50 @@ export async function generateMetadata(
   { params }: { params: Promise<{ slug: string }> }
 ): Promise<Metadata> {
   const { slug } = await params;;
-  const post = await getPostBySlugFromDB(slug);
-  if (!post) return {};
+  try {
+    const blog = await getPostBySlugFromDB(slug);
+    if (!blog) return {};
 
-  const img = post.image?.featuresImage;
+    if (!blog || blog.status === "Unpublish") {
+      return {
+        title: "Post Not Found",
+        robots: { index: false, follow: false }, // Prevent search engines from indexing missing pages
+      };
+    }
 
-  return {
-    title: `${post.seoTitle || post.postTitle}`,
-    description: post.seoDescription || post.shortDescription,
-    keywords: post.seoKeyword,
-    alternates: {
-      canonical: `https://shikderambulance.com/${post.slug}`,
-    },
-    openGraph: {
-      url: `${BASE_URL}/${post.slug}`,
-      images: img ? [img] : [],
-    },
-  };
+    // Default fallbacks if custom SEO settings aren't filled out in the dashboard form
+    const finalTitle = blog.seoTitle || `${blog.postTitle} | My Blog`;
+    const finalDescription = blog.seoDescription || blog.shortDescription || `Read about ${blog.postTitle}`;
+    const featureImageUrl = blog.image?.featuresImage || blog.image?.thumbnail || "";
+    const url = `${BASE_URL}/${blog?.slug}`
+
+    return {
+      title: finalTitle,
+      description: finalDescription,
+      keywords: blog.seoKeyword || [],
+      alternates: {
+        canonical: url, // Critical for avoiding duplicate URL penalties
+      },
+      openGraph: {
+        title: finalTitle,
+        description: finalDescription,
+        type: "article",
+        publishedTime: new Date(blog.publishDate).toISOString(),
+        authors: "Shikder Ambulance",
+        images: featureImageUrl ? [{ url: featureImageUrl }] : [],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: finalTitle,
+        description: finalDescription,
+        images: featureImageUrl ? [featureImageUrl] : [],
+      },
+    };
+  } catch (error) {
+    return {
+      title: "Blog Post",
+    };
+  }
 }
 
 const BlogDetailsPage = async (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta charSet="UTF-8" />
+        {/* <meta charSet="UTF-8" /> */}
         {/* <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=5"
